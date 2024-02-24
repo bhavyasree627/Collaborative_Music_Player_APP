fd05f3c310b6557b43be91fe7d792a9ecc84f708
Bhavya <bhogathibhavya99@gmail.com>
2024-02-24 17:47:40 +0530

    Added the router functionality from react

--- music_player/frontend/src/components/App.js
+++ music_player/frontend/src/components/App.js
@@ -1,19 +1,20 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
+import HomePage from "./HomePage";
 
 export default class App extends Component {
-    constructor(props) {
-        super(props);
-    }
+  constructor(props) {
+    super(props);
+  }
 
-    render() {
-        return (
-            <h1>Hello welcome to React+Django</h1>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <HomePage />
+      </div>
+    );
+  }
 }
 
-//adding the component to the templates>frontend>index.html file.
-const appDiv = document.getElementById('app');
-render(<App />,appDiv)
-
+const appDiv = document.getElementById("app");
+render(<App />, appDiv);
